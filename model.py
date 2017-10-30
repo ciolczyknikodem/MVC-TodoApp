163cf9087d228ccbc4fedccbe5f2106a1bcff377
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2017-10-30 18:54:44 +0100

    Add constructor(__init__) to model.py, define class ToDo.

---
+++ model.py
@@ -0,0 +1,8 @@
+class ToDo:
+
+    def __init__(self, _id, name, description):
+
+        self.id = _id
+        self.name = name
+        self.description = description
+        self. is_done = False
