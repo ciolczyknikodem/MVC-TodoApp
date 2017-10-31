7c8c08d0226d4d3f1e3fd2a31426c2bd25236914
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2017-10-31 11:49:01 +0100

    Add str method to class model.py, return formated string

--- model.py
+++ model.py
@@ -6,3 +6,15 @@ class ToDo:
         self.name = name
         self.description = description
         self. is_done = False
+
+    def set_to_done(self):
+        self.is_done = True
+
+    def __str__(self):
+        if self.is_done:
+            infromation_todo = '[X]' + self.id + ': ' + self.name + '\n' + self.description
+            return infromation_todo
+
+        else:
+            infromation_todo = '[X]' + self.id + ': ' + self.name + '\n' + self.description
+            return infromation_todo
