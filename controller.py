f1186e8844294b5d00ea0bab92ea6990a4eee8e8
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2017-11-01 15:08:21 +0100

    Change logci of while loop during addin task

--- controller.py
+++ controller.py
@@ -6,11 +6,11 @@ def add_task_to_list():
     name_of_new_task = ''
     description_of_task = ''
 
-    while len(name_of_new_task) > 0 and len(name_of_new_task) < 20:
+    while not len(name_of_new_task) > 0 and len(name_of_new_task) < 20:
         view.display_add_task_name()
         name_of_new_task = input()
 
-    while len(description_of_task) > 0 and len(name_of_new_task) < 150:
+    while not len(description_of_task) > 0 and len(name_of_new_task) < 150:
         view.display_add_task_description()
         description_of_task = input()
 
