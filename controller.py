dd365cde4a9d6f709210de55ba14c9870b7daf87
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2017-11-03 14:50:09 +0100

    Refactored of adding task name and description, made 2 universal function of adding name and descritpion

--- controller.py
+++ controller.py
@@ -8,34 +8,59 @@ def back_to_menu():
 
 
 def add_task_to_list(todo_list):
-    name_of_new_task = ''
-    description_of_task = ''
     unique_id = True
-    min_len_of_task = 0
-    max_len_of_name = 20
-    max_len_of_description = 150
 
     while unique_id is True:
         new_id = generate_id_for_task()
         unique_id = todo_list.check_if_id_already_exist(new_id)
 
-    while not len(name_of_new_task) > min_len_of_task and len(name_of_new_task) < max_len_of_name:
-        view.display_add_task_name()
-        name_of_new_task = input()
-
-    while not len(description_of_task) > min_len_of_task and len(name_of_new_task) < max_len_of_description:
-        view.display_add_task_description()
-        description_of_task = input()
+    name_of_new_task = add_name()
+    description_of_task = add_description()
 
     new_task = ToDo(new_id, name_of_new_task, description_of_task)
     todo_list.add_task_to_list(new_task)
 
 
+def add_name():
+    name_of_new_task = ''
+    min_len_of_task = 0
+    max_len_of_name = 20
+    while not len(name_of_new_task) > min_len_of_task and len(name_of_new_task) < max_len_of_name:
+        view.display_add_task_name()
+        name_of_new_task = input()
+
+    return name_of_new_task
+
+
+def add_description():
+    description_of_task = ''
+    min_len_of_task = 0
+    max_len_of_description = 150
+    while not len(description_of_task) > min_len_of_task and len(description_of_task) < max_len_of_description:
+        view.display_add_task_description()
+        description_of_task = input()
+
+    return description_of_task
+
+
 def modify_task(todo_list):
-    pass
+    task_index = get_task_index(todo_list)
+    task_to_change = todo_list.get_task(task_index)
+
+    change_description = 'description'
+    change_name = 'name'
+    view.display_modify_todo()
+    type_of_change = input()
+
+    if type_of_change == change_description:
+        view.display_modify_todo(change_description)
+
+    elif type_of_change == change_name:
+        view.display_modify_todo(change_name)
 
 
-def get_task_index():
+def get_task_index(todo_list, operation='change'):
+    view.display_get_index_of_task(todo_list, operation)
     change_input_index_by_1 = 1
     correct_input = True
     while correct_input is True:
@@ -49,8 +74,8 @@ def get_task_index():
 
 
 def mark_task(todo_list):
-    view.display_all_tasks_details(todo_list)
-    index_of_task = get_task_index()
+    operation = 'mark'
+    index_of_task = get_task_index(todo_list, operation)
     is_error = todo_list.mark_task(index_of_task)
     if is_error:
         view.display_error()
@@ -58,8 +83,8 @@ def mark_task(todo_list):
 
 
 def remove_task_from_list(todo_list):
-    view.display_all_tasks_details(todo_list)
-    index_of_task = get_task_index()
+    operation = 'remove'
+    index_of_task = get_task_index(todo_list, operation)
     is_error = todo_list.remove_task_from_list(index_of_task)
     if is_error:
         view.display_error()
